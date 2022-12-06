d2b18406e6531543cff74fca4a4d5b6dfdbb6b1c
xxx <humengmingxx@gmail.com>
2022-12-06 10:57:19 +0800

    feat：add comment

--- halo/hello/hello.rs
+++ halo/hello/hello.rs
@@ -3,4 +3,6 @@ fn main() {
     println!("hello rust")
     // rustc hello.rs >>> hello 生成二进制的可执行文件
     // ./hello  >>> 打印出 hello rust
+    // 在 rust 中 宏 都是以 ！结尾的，以 ！ 结尾的函数都是宏调用
+    // 我们可以通过 宏 来进行 元编程，rust 中的 宏类似于 JS 中的函数，宏 可以理解为 函数的增强版
 }
