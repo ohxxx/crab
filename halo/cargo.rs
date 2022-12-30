bd81b34c950e28e0e94f8a8f8caccded7981196d
xxx <humengmingxx@gmail.com>
2022-12-30 16:42:00 +0800

    feat: cargo

---
+++ halo/cargo.rs
@@ -0,0 +1,10 @@
+fn main() {
+    // cargo 是 rust 的包管理工具，它可以帮助我们管理 rust 项目的依赖
+    // 常用命令：
+    // cargo new 项目名 >>> 创建一个新项目
+    // cargo check >>> 分析当前项目并报告项目中的错误，但不会编译任何项目文件
+    // cargo build >>> 编译当前项目
+    // cargo run >>> 编译并运行当前项目
+    // cargo clean >>> 移除当前项目下 target 目录中的所有子项目和文件
+    // cargo update >>> 更新当前项目中 Cargo.lock 中的依赖
+}
