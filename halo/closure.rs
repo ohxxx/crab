757fefc5ea3507352690c057d61d331ca6dab281
xxx <humengmingxx@gmail.com>
2022-12-31 18:21:07 +0800

    feat: closure

--- halo/closure.rs
+++ halo/closure.rs
@@ -15,13 +15,13 @@ fn main() {
 
     // let 闭包变量 = |参数列表| { ... };
 
-    let double = |x| x * 2;
-    let add = |x, y| x + y;
-    println!("double(2) = {}", double(2));
-    println!("add(2, 3) = {}", add(2, 3));
-    let v = 3;
-    let add_v = |x| x + v;
-    println!("add_v(2) = {}", add_v(2));
+    // let double = |x| x * 2;
+    // let add = |x, y| x + y;
+    // println!("double(2) = {}", double(2));
+    // println!("add(2, 3) = {}", add(2, 3));
+    // let v = 3;
+    // let add_v = |x| x + v;
+    // println!("add_v(2) = {}", add_v(2));
 
     // 捕获
     // 闭包本质上很灵活，闭包可以在没有类型标注的情况下运行，可以移动，借用
@@ -36,4 +36,61 @@ fn main() {
     // 3、闭包虽然没有函数名，但可以把整个闭包赋值一个变量，通过调用该变量来完成闭包的调用
     // 4、闭包不用声明返回值，但它可以有返回值，而且使用最后一条语句的执行结果作为返回值，闭包的返回值可以赋值给一个变量
     // 5、闭包又称为内联函数，可以让闭包访问外层函数里的变量
+
+    // 闭包使用
+    let add = |x, y| x + y;
+    let res = add(1, 2);
+    println!("res = {}", res);
+
+    receives_closure(add);
+
+    let y = 22;
+    receives_closure2(|x| x + y);
+
+    let clo = returns_closure();
+    println!("返回闭包 = {}", clo(6));
+
+    let rr_clo = rr_closure(add, 3);
+    println!("rr_clo = {}", rr_clo(3));
+
+    let rr_clo2 = rr_closure2(add, 6);
+    println!("rr_clo2 = {}", rr_clo2(3));
+}
+
+fn receives_closure<F>(closure: F)
+where
+    F: Fn(i32, i32) -> i32,
+{
+    let res = closure(1, 2);
+    println!("闭包作为参数执行的结果 = {}", res);
+}
+
+// 闭包捕获变量
+fn receives_closure2<F>(closure: F)
+where
+    F: Fn(i32) -> i32,
+{
+    let res = closure(1);
+    println!("闭包作为参数执行的结果 = {}", res);
+}
+
+// 返回闭包
+fn returns_closure() -> impl Fn(i32) -> i32 {
+    |x| x + 6
+}
+
+// 参数和返回值都有闭包
+fn rr_closure<F>(f: F, x: i32) -> impl Fn(i32) -> i32
+where
+    F: Fn(i32, i32) -> i32,
+{
+    move |y| f(x, y)
+}
+
+fn rr_closure2<F, X, Y, Z>(f: F, x: X) -> impl Fn(Y) -> Z
+where
+    F: Fn(X, Y) -> Z,
+    X: Copy,
+{
+    move |y| f(x, y)
 }
