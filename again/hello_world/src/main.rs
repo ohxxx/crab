a1fae074b86d360a4d557abd888545ac27b3bca1
xxx <humengmingxx@gmail.com>
2023-01-04 16:29:41 +0800

    feat: halo

---
+++ again/hello_world/src/main.rs
@@ -0,0 +1,13 @@
+fn greet_halo() {
+    let southern_germany = "Grüß Gott!";
+    let chinese = "世界，你好";
+    let english = "halo world";
+    let regions = [southern_germany, chinese, english];
+    for region in regions.iter() {
+        println!("{}", &region);
+    }
+}
+
+fn main() {
+    greet_halo();
+}
